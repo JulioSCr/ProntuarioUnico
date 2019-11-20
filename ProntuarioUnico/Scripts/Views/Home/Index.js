8332f3410b42be86d87fa3e265678f23a32768e2
JULIO CRUZ <julio.cruz3@fatec.sp.gov.br>
2019-11-20 19:30:27 -0300

    Tela: Prontuário Descrição: Transformando numa página dinâmica

--- ProntuarioUnico/Scripts/Views/Home/Index.js
+++ ProntuarioUnico/Scripts/Views/Home/Index.js
@@ -1,5 +1,30 @@
 ﻿var Index = window.Index || {
-
+    marrDataSource_TbProntuario: [
+        {
+            Atendimento_NR: 17896,
+            Atendimento_TP: 'Consulta',
+            Atendimento_DT: '15/11/2019 11:30',
+            Medico_NM: 'Dr. Jorge Neto',
+            Medico_Especialidade: 'Cirurgião gastro',
+            Atendimento_DS: 'Casos de dores abdominais crônicas indicativos de gastrite, requisição de exames.Casos de dores abdominais crônicas indicativos de gastrite, requisição de exames.'
+        },
+        {
+            Atendimento_NR: 17523,
+            Atendimento_TP: 'Exame',
+            Atendimento_DT: '13/11/2019 14:00',
+            Medico_NM: 'Dr William Saad',
+            Medico_Especialidade: 'Cirurgião gastro',
+            Atendimento_DS: 'Verificação de pedras na vesícula.'
+        },
+        {
+            Atendimento_NR: 17000,
+            Atendimento_TP: 'Exame',
+            Atendimento_DT: '12/11/2019 14:00',
+            Medico_NM: 'Dr William Saad',
+            Medico_Especialidade: 'Cirurgião gastro',
+            Atendimento_DS: 'Verificação de gatrite.'
+        }
+    ]
 }
 
 Index.MontarTela = function () {
@@ -12,6 +37,9 @@ Index.MontarTela = function () {
 
 Index.CriarComponentes = function () {
     try {
+        // Carrega os dados da tabela
+        Index.GerarTbProntuario(Index.marrDataSource_TbProntuario);
+        // Ação de clique nas linhas da tabela
         $('.cssIndex_TbProntuario_Row').click(function () {
             Index.TbProntuario_Expandir($(this)[0]);
         });
@@ -29,6 +57,27 @@ Index.TbProntuario_Expandir = function (vobjElmento) {
             $(vobjElmento.nextElementSibling).css({ display: 'table-row' })
             vobjElmento.children[3].children[0].children[0].src = gstrGlobalPath + '/Content/Imagens/Home/imgUp.png'
         }
+    } catch (ex) {
+        alert(ex);
+    }
+}
+
+Index.GerarTbProntuario = function (varrDataSource) {
+    var lobjProntuario_Template = new Object();
+    var lobjProntuario = new Object();
+    try {
+
+        function render(props) {
+            return function (tok, i) {
+                return (i % 2) ? props[tok] : tok;
+            };
+        }
+
+        // Gerar template
+        lobjProntuario_Template = $('script[data-template="tmpIndex_TbProntuario_Rows"]').text().split(/\$\{(.+?)\}/g);
+        $('#tblIndex_TbProntuario tbody').append(varrDataSource.map(function (lobjProntuario) {
+            return lobjProntuario_Template.map(render(lobjProntuario)).join('');
+        }));
     } catch (ex) {
         alert(ex);
     }
