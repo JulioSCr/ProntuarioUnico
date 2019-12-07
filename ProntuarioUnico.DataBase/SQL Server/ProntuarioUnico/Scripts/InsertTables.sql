72f7620a09e433f6d8a7d03b6e4b9f72492c4d99
JULIO CRUZ <julio.cruz3@fatec.sp.gov.br>
2019-12-07 02:21:18 -0300

    Novo script de insert

--- ProntuarioUnico.DataBase/SQL Server/ProntuarioUnico/Scripts/InsertTables.sql
+++ ProntuarioUnico.DataBase/SQL Server/ProntuarioUnico/Scripts/InsertTables.sql
@@ -1,47 +1,82 @@
 USE [ProntuarioUnico]
 GO
 
-INSERT INTO dbo.TIPO_DOCUMENTO
-           (DS_TIPO_DOCUMENTO)
-     VALUES
-           ('Carteira de Identidade - RG'),
-		   ('Cadastro de Pessoa Física - CPF'),
-		   ('Carteira Nacional de Habilitação - CNH'),
-		   ('Passaporte'),
-		   ('Carteira de Registro Nacional Migratório - CRNM')
-GO
-
-INSERT INTO dbo.TIPO_ATENDIMENTO 
-			(DS_TIPO_ATENDIMENTO)
-	VALUES 
-			('Consulta'), 
-			('Exame')
-
-
-INSERT INTO dbo.ESPECIALIDADE_ATENDIMENTO 
-			(DS_ESPECIALIDADE)
-     VALUES
-           ('Dermatologia'),
-		   ('Ortopedia'),
-		   ('Psiquiatria'),
-		   ('Oftalmologia'),
-		   ('Cardiologia'),
-		   ('Urologia')
-
-INSERT INTO [dbo].[PESSOA_FISICA]
-           ([NM_PESSOA_FISICA]
-           ,[DT_NASCIMENTO]
-           ,[DS_TELEFONE]
-           ,[CD_TIPO_DOCUMENTO]
-           ,[VL_TIPO_DOCUMENTO]
+INSERT INTO [dbo].[TIPO_ATENDIMENTO]
+           ([DS_TIPO_ATENDIMENTO]
            ,[IE_ATIVO]
            ,[DT_ATUALIZACAO])
      VALUES
-           ('André Matias'
-           ,'1998-12-25'
-           ,'1155322139'
-           ,2
-           ,'12345695246'
+           ('Consultas'
+           ,1
+           ,GETDATE())
+GO
+
+INSERT INTO [dbo].[TIPO_ATENDIMENTO]
+           ([DS_TIPO_ATENDIMENTO]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
+     VALUES
+           ('Exame'
+           ,1
+           ,GETDATE())
+GO
+
+INSERT INTO [dbo].[ESPECIALIDADE_ATENDIMENTO]
+           ([DS_ESPECIALIDADE]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
+     VALUES
+           ('Dermatologia'
+           ,1
+           ,GETDATE())
+GO
+
+INSERT INTO [dbo].[ESPECIALIDADE_ATENDIMENTO]
+           ([DS_ESPECIALIDADE]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
+     VALUES
+           ('Ortopedia'
+           ,1
+           ,GETDATE())
+GO
+
+INSERT INTO [dbo].[ESPECIALIDADE_ATENDIMENTO]
+           ([DS_ESPECIALIDADE]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
+     VALUES
+           ('Psiquiatria'
+           ,1
+           ,GETDATE())
+GO
+
+INSERT INTO [dbo].[ESPECIALIDADE_ATENDIMENTO]
+           ([DS_ESPECIALIDADE]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
+     VALUES
+           ('Oftalmologia'
+           ,1
+           ,GETDATE())
+GO
+
+INSERT INTO [dbo].[ESPECIALIDADE_ATENDIMENTO]
+           ([DS_ESPECIALIDADE]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
+     VALUES
+           ('Cardiologia'
+           ,1
+           ,GETDATE())
+GO
+
+INSERT INTO [dbo].[ESPECIALIDADE_ATENDIMENTO]
+           ([DS_ESPECIALIDADE]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
+     VALUES
+           ('Urologia'
            ,1
            ,GETDATE())
 GO
@@ -50,16 +85,34 @@ INSERT INTO [dbo].[PESSOA_FISICA]
            ([NM_PESSOA_FISICA]
            ,[DT_NASCIMENTO]
            ,[DS_TELEFONE]
-           ,[CD_TIPO_DOCUMENTO]
-           ,[VL_TIPO_DOCUMENTO]
+           ,[NR_CPF]
+           ,[DS_SENHA]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
+     VALUES
+           ('André Matias'
+           ,'1998-12-25'
+           ,'1155322139'
+           ,'12345695246'
+           ,'teste'
+           ,1
+           ,GETDATE())
+GO
+
+INSERT INTO [dbo].[PESSOA_FISICA]
+           ([NM_PESSOA_FISICA]
+           ,[DT_NASCIMENTO]
+           ,[DS_TELEFONE]
+           ,[NR_CPF]
+           ,[DS_SENHA]
            ,[IE_ATIVO]
            ,[DT_ATUALIZACAO])
      VALUES
            ('Robson Mauricio'
            ,'1990-02-13'
            ,'1142316548'
-           ,2
            ,'45698725482'
+           ,'teste'
            ,1
            ,GETDATE())
 GO
@@ -67,11 +120,15 @@ GO
 INSERT INTO [dbo].[MEDICO]
            ([CRM_MEDICO]
            ,[CD_PESSOA_FISICA]
+           ,[NM_GUERRA]
+           ,[DS_SENHA]
            ,[IE_ATIVO]
            ,[DT_ATUALIZACAO])
      VALUES
            (52715573
-           ,3
+           ,1
+           ,'Dr Robson Mauricio'
+           ,'teste'
            ,1
            ,GETDATE())
 GO
@@ -82,28 +139,22 @@ INSERT INTO [dbo].[ATENDIMENTO_PACIENTE]
            ,[ID_TIPO_ATENDIMENTO]
            ,[ID_ESPECIALIDADE]
            ,[DT_ATENDIMENTO]
-           ,[DS_OBSERVACAO])
+           ,[DS_SINTOMA]
+           ,[DS_DIAGNOSTICO]
+           ,[DS_PRESCRICAO]
+           ,[DS_OBSERVACAO]
+           ,[IE_ATIVO]
+           ,[DT_ATUALIZACAO])
      VALUES
-           (2
+           (1
            ,52715573
            ,1
-           ,3
+           ,5
            ,GETDATE()
-           ,'Casos de dores abdominais.')
-GO
-
-INSERT INTO [dbo].[ATENDIMENTO_PACIENTE]
-           ([CD_PESSOA_FISICA]
-           ,[CRM_MEDICO]
-           ,[ID_TIPO_ATENDIMENTO]
-           ,[ID_ESPECIALIDADE]
-           ,[DT_ATENDIMENTO]
-           ,[DS_OBSERVACAO])
-     VALUES
-           (2
-           ,52715573
-           ,2
-           ,3
-           ,GETDATE()
-           ,'Consulta para verificar dores abdominais.')
+           ,'Calor intenso e sufocameno.'
+           ,'Pressão alta'
+           ,'Remédio para pressão alta'
+           ,'Medir a pressão'
+           ,1
+           ,GETDATE())
 GO
